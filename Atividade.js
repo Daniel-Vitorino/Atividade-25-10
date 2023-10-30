b970b75b63edcad5fab1b7642ec433243086097a
Daniel Vitorino <danielvitorino001@gmail.com>
2023-10-30 11:12:02 -0300

    Atividade 25-10

---
+++ Atividade.js
@@ -0,0 +1,26 @@
+function Enviar() {
+    alert("Suas informações e sua mensagem foram enviadas com sucesso !");
+}
+
+let personagens = document.getElementsByClassName("Personagens");
+
+console.log(personagens);
+console.log(personagens[0]);
+console.log(personagens[1]);
+console.log(personagens[2]);
+console.log(personagens[3]);
+
+
+let filmes = document.getElementsByClassName("Filmes");
+
+console.log(filmes);
+console.log(filmes[0]);
+console.log(filmes[1]);
+console.log(filmes[2]);
+console.log(filmes[3]);
+console.log(filmes[4]);
+console.log(filmes[5]);
+console.log(filmes[6]);
+
+let contato = document.getElementById("Contato");
+
